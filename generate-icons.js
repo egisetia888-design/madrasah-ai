21af275b992b5ef0a3930be37503e0f38b5c5dec
Egi Setiawan <egisetia8.8.8@gmail.com>
2026-07-31 21:01:19 +0700

    chore: update icon filenames and apple-touch-icon

--- generate-icons.js
+++ generate-icons.js
@@ -7,12 +7,17 @@ async function generate() {
   await sharp(svgBuffer)
     .resize(192, 192)
     .png()
-    .toFile('public/icon-192.png');
+    .toFile('public/icon-192x192.png');
     
   await sharp(svgBuffer)
     .resize(512, 512)
     .png()
-    .toFile('public/icon-512.png');
+    .toFile('public/icon-512x512.png');
+
+  await sharp(svgBuffer)
+    .resize(180, 180)
+    .png()
+    .toFile('public/apple-touch-icon.png');
     
   console.log('Icons generated successfully.');
 }
